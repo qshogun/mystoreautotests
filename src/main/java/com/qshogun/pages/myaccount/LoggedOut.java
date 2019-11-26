2efd051c3102fb9fbf59b2bb591b3918d8e6a992
qshogun <55805051+qshogun@users.noreply.github.com>
2019-11-26 02:21:28 +0100

    Created com.qshogun.pages.myaccount package Created .myaccount LoggedOut class

---
+++ src/main/java/com/qshogun/pages/myaccount/LoggedOut.java
@@ -0,0 +1,19 @@
+package com.qshogun.pages.myaccount;
+
+import com.qshogun.common.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoggedOut extends BasePage {
+
+    
+
+
+
+
+
+    public LoggedOut(WebDriver driver) {
+        super(driver);
+    }
+}
