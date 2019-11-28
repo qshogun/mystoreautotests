d96268ea482f066aeaa6c46c1398d5c671b1192f
qshogun <55805051+qshogun@users.noreply.github.com>
2019-11-28 19:19:56 +0100

    Created DataGenerator to generate user credentials

---
+++ src/main/java/com/qshogun/util/DataGenerator.java
@@ -0,0 +1,25 @@
+package com.qshogun.util;
+
+import com.github.javafaker.Faker;
+
+public class DataGenerator {
+
+    private static Faker faker = new Faker();
+
+    public static String getFirstname() {
+        return faker.name().firstName();
+    }
+
+    public static String getLastname() {
+        return faker.name().lastName();
+    }
+
+    public static String getEmailAddress() {
+        return faker.internet().emailAddress();
+    }
+
+    public static String getPassword() {
+        return faker.internet().password();
+    }
+
+}
