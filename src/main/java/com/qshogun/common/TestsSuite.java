4cc537ded9a880016dc7796ee9917e7a14fd0d1a
qshogun <55805051+qshogun@users.noreply.github.com>
2019-11-30 14:21:28 +0100

    Created TestsSuite to handle @Before and @After tests

--- src/main/java/com/qshogun/common/TestsSuite.java
+++ src/main/java/com/qshogun/common/TestsSuite.java
@@ -1,4 +1,26 @@
 package com.qshogun.common;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestsSuite {
+
+    public WebDriver driver;
+
+    @Before
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("http://5.196.7.235/");
+    }
+
+    @After
+    public void after() {
+        driver.quit();
+    }
 }
